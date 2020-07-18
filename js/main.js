814361e769e6a748e8b026143f588bb71d41b6e1
baubrun <baubelf@gmail.com>
2020-07-17 20:25:58 -0400

    added jquery

--- js/main.js
+++ js/main.js
@@ -1,20 +1,56 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-    // rRipples
-    $("#header, .info").ripples({
-        dropRadius: 25,
-        perturbance: 0
-      });
+  $(".navbar-toggler").click(function () {
+    $(".navbar-toggler").toggleClass("change")
+  })
 
-      //img magnigic-popup
-      $(".img-gallery-popup-container").magnificPopup({
-        delegate: "a",
-        type: "image",
-        gallery: {
-          enabled: true,
-          type: "image"
-        }
-      })
+  // sticky nav
+  $(window).scroll(function () {
+    let pos = $(this).scrollTop()
+    if (pos >= 718) {
+      $(".navbar").addClass("navbar-background")
+      $(".navbar").addClass("fixed-top")
+    } else {
+      $(".navbar").removeClass("navbar-background")
+      $(".navbar").removeClass("fixed-top")
+    }
+  })
+
+  // sticky arrow scroll-to-top 
+  $(window).scroll(function () {
+    let pos = $(this).scrollTop()
+    if (pos >= 718) {
+      $("#to-top").addClass("scroll-to-top ")
+    } else {
+      $("#to-top").removeClass("scroll-to-top ")
+    }
+  })
 
 
+  // smooth scroll
+  $(".nav-item a, .header-link, #to-top").click(function (evt) {
+    evt.preventDefault()
+
+    let target = $(this).attr("href")
+    $("html, body").stop().animate({
+      scrollTop: $(target).offset().top - 25
+    }, 3000)
+  })
+
+  // Ripples
+  $("#header, .info").ripples({
+    dropRadius: 25,
+    perturbance: 0
+  });
+
+
+  //img magnific-popup
+  $(".img-gallery-popup-container").magnificPopup({
+    delegate: "a",
+    type: "image",
+    gallery: {
+      enabled: true,
+      type: "image"
+    }
+  })
 })
